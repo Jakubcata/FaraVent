f395caa02ea4b6ee3051bde7a38a0793ffa6e19a
Michal <michal.korbela@kiwi.com>
2020-05-14 20:01:51 +0200

    Ajax send message

--- faravent-admin/resources/views/mqtt/messages.blade.php
+++ faravent-admin/resources/views/mqtt/messages.blade.php
@@ -4,12 +4,30 @@
 
         <form action="{{route('publish')}}">
           <div class="input-group">
-            <input type="text" name="topic" placeholder="topic" class="form-control">
-            <input type="text" name="message" placeholder="message" class="form-control">
-            <button class="btn btn-primary">Send</button>
+            <input type="text" name="topic" id="send-message-topic" placeholder="topic" class="form-control">
+            <input type="text" name="message" id="send-message-message" placeholder="message" class="form-control">
+            <button class="btn btn-primary" id="send-message">Send</button>
           </div>
         </form>
     </div>
+    <script>
+    $(function(){
+        $("#send-message").click(function(e){
+            e.preventDefault();
+            $.get({
+                url:"{{route('publish')}}",
+                data:{
+                    topic:$("#send-message-topic").val(),
+                    message:$("#send-message-message").val(),
+                }
+            });
+            $("#send-message-topic").val("");
+            $("#send-message-message").val("");
+        });
+    });
+
+    </script>
+
 </div>
 
 <div class="main-card mb-3 card">
@@ -18,23 +36,23 @@
         <script>
         var timerId= 0;
         $(function(){
-          $("#refresh_messages").click(function(){
-            if($("#refresh_messages" ).is(":checked")) {
-                timerId = setInterval(function () {
-                  $.ajax({
-                      type: "get",
-                      url: "{{route('lastMessagesSnippet')}}",
-                      cache: false,
-                      success: function (html) {
-                          $("#last-messages-table").html(html);
-                      }
-                  });
-              }, 1000);
-          } else{
-
-            clearInterval(timerId);
-          }
-      })});
+            $("#refresh_messages").click(function(){
+                if($("#refresh_messages" ).is(":checked")) {
+                    timerId = setInterval(function () {
+                        $.ajax({
+                            type: "get",
+                            url: "{{route('lastMessagesSnippet')}}",
+                            cache: false,
+                            success: function (html) {
+                                $("#last-messages-table").html(html);
+                            }
+                        });
+                    }, 3000);
+                } else{
+                    clearInterval(timerId);
+                }
+            })
+        });
         </script>
         @include('mqtt.messages_table')
     </div>
