2fddb84d0b8c98a1eb441c39f5a677603b6142f5
Michal <michal.korbela@kiwi.com>
2020-05-17 13:18:23 +0200

    phone app

--- faravent-admin/resources/views/layout.blade.php
+++ faravent-admin/resources/views/layout.blade.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
     <meta name="description" content="This is an example dashboard created using build-in elements and components.">
     <meta name="msapplication-tap-highlight" content="no">
+    <link rel="manifest" href="/manifest.json">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
